1a4540f6e6ce5ee34a1364224ed0851cacc68a22
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-18 15:13:58 -0700

    Major refactoring.

--- wid-card-dispatch-report.js
+++ wid-card-dispatch-report.js
@@ -1,5 +1,79 @@
 'use strict';
 
+Polymer({
+  is: 'person-header',
+  properties: {
+    name: String,
+    icon: String,
+    odd: {
+      type: Boolean,
+      value: false
+    }
+  },
+  _iconPath: function _iconPath(value) {
+    return '/images/cards/avatars/' + value;
+  },
+  _avatarClass: function _avatarClass(isOdd) {
+    var ret = 'person-avatar flex-3 layout vertical center-center ';
+    if (isOdd === true) {
+      ret += 'person-avatar-odd';
+    }
+    return ret;
+  },
+  _nameClass: function _nameClass(isOdd) {
+    var ret = 'person-name flex layout vertical center-center ';
+    if (isOdd === true) {
+      ret += 'person-name-odd ';
+    }
+    return ret;
+  }
+
+});
+Polymer({
+  is: 'month-header',
+  properties: {
+    month: String,
+    odd: {
+      type: Boolean,
+      value: false
+    }
+  },
+  _monthAbbrev: function _monthAbbrev(month) {
+    if (month.length > 4) {
+      return month.substr(0, 3);
+    } else {
+      return month;
+    }
+  },
+  _mainClass: function _mainClass(isOdd) {
+    var ret = 'flex layout horizontal ';
+    if (isOdd === true) {
+      ret += 'odd';
+    }
+    return ret;
+  }
+
+});
+Polymer({
+  is: 'person-cell',
+  properties: {
+    aiBinding: Object,
+    erBinding: Object,
+    miBinding: Object,
+    odd: {
+      type: Boolean,
+      value: false
+    }
+  },
+  _mainClass: function _mainClass(isOdd) {
+    var ret = 'flex person-column layout vertical ';
+    if (isOdd === true) {
+      ret += 'odd';
+    }
+    return ret;
+  }
+
+});
 (function () {
 
   Polymer({
